b7e17bfbc5fe88bbf714cbf7782be78aa5c54b82
Filip Ignaciuk <250965@edu.p.lodz.pl>
2024-06-20 22:50:09 +0200

    Square added

---
+++ klasy.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <string>
+#include <cmath>
+using namespace std;
+
+class Figure {
+public:
+    // Czysta wirtualna metoda area()
+    virtual double area() const = 0;
+
+    // Czysta wirtualna metoda show()
+    virtual void show() const = 0;
+
+    // Wirtualny destruktor
+    virtual ~Figure() = default;
+};
+
+class Square : public Figure{
+private:
+    double side;
+public:
+    Square(double s) : side(s) {}
+
+    double area() const override {
+        return side * side;
+    }
+
+    void show() const override {
+        std::cout << "Square with side lenght: " << side << " and area: " << area()
+                  << std::endl;
+    }
+};
+
+class Rectangle : public Figure {
+private:
+    double width, height;
+
+public:
+    Rectangle(double w, double h) : width(w), height(h) {}
+
+    double area() const override {
+        return width * height;
+    }
+
+    void show() const override {
+        std::cout << "Rectangle with width: " << width << ", height: " << height << " and area: " << area()
+                  << std::endl;
+    }
+};
+
+class Circle : public Figure {
+private:
+    double radius;
+
+public:
+    Circle(double r) : radius(r) {}
+
+    double area() const override {
+        return M_PI * radius * radius;
+    }
+
+    void show() const override {
+        std::cout << "Circle with radius: " << radius << " and area: " << area() << std::endl;
+    }
+};
